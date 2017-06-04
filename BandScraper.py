dc61999bfae481e1c43cece201e42457971b7499
Kabur <tomasfabry1@gmail.com>
2017-06-05 00:35:47 +0200

    Google found out I'm a robot :( all done, just need to test if Haken works(Studio albums is below Demo)

--- BandScraper.py
+++ BandScraper.py
@@ -49,17 +49,23 @@ for band in bands:
     soup = bs.BeautifulSoup(response.text, "lxml")
 
     link = soup.find('h3', attrs={'class': 'r'}) # Guides BS to h3 class "r" where green Wikipedia URLs are located, then prints URLs
+    # print(soup.prettify())
+
     link = link.find("a").get("href")
     link = link[7:].split("&")[0] # [7:] strips the /url?q= prefix, split removes garbage in link after &
     source = requests.get(link)
     soup = bs.BeautifulSoup(source.content, 'lxml')
 
-    ''' find discography element '''
+    # ''' find discography element '''
     discography = soup.find(id='Discography')
     disco_parent = discography.find_parent()
+    # sibling = disco_parent.find_next_sibling()
+    # ''' Go sideways to find the table with albums '''
+    # while sibling is not None and sibling.name != "table":
+    #     sibling = sibling.find_next_sibling()
+
     sibling = disco_parent.find_next_sibling()
-    ''' Go sideways to find the table with albums '''
-    while sibling is not None and sibling.name != "table" and sibling.name != "div":
+    while re.match('.*[aA]lbums.*', sibling.find('span').get_text()) is None:
         sibling = sibling.find_next_sibling()
 
     ''' 
@@ -73,25 +79,30 @@ for band in bands:
             table_rows = table_rows[:-2]
 
         th = table_rows[-2].find('th')
-        # ''' if there is <a>, extract the album from there '''
+        # if its in <a>
         if th.find('i').find("a") is not None:
             band_dict['album1'] = th.find('i').find("a").get('title')
-            # ''' if there is no <a>, extract the album from <i> '''
+            # if its just in <i>
         else:
             band_dict['album1'] = th.find('i').get_text()
         td = th.find_next_sibling()
         band_dict['year1'] = re.findall('\d{4}', td.get_text())[0]
 
         # ''' same shit for the second album, should put it to a function prolly xD '''
-        th = table_rows[-4].find('th')
-        # ''' if there is <a>, extract the album from there '''
-        if th.find('i').find("a") is not None:
-            band_dict['album2'] = th.find('i').find("a").get('title')
-            # ''' if there is no <a>, extract the album from <i> '''
-        else:
-            band_dict['album2'] = th.find('i').get_text()
-        td = th.find_next_sibling()
-        band_dict['year2'] = re.findall('\d{4}', td.get_text())[0]
+        try:
+            th = table_rows[-4].find('th')
+            # if its in <a>
+            if th.find('i').find("a") is not None:
+                band_dict['album2'] = th.find('i').find("a").get('title')
+                # if its just in <i>
+            else:
+                band_dict['album2'] = th.find('i').get_text()
+            td = th.find_next_sibling()
+            band_dict['year2'] = re.findall('\d{4}', td.get_text())[0]
+        except IndexError as e:
+            band_dict['year2'] = 'NONE'
+            band_dict['album2'] = 'NONE'
+            print(str(e))
 
         ''' 
         TYPE 2:
@@ -106,26 +117,43 @@ for band in bands:
             print("DIDNT FIND AN ALBUM, INSPECT WIKI")
         else:
             lis = sibling.contents
-            if lis[-2].find('a') is not None:  # if its in <a>...
+
+            # sometimes there is a junk tag instead of the year -> cut
+            if re.match('.*\d{4}.*', str(lis[-2].contents[1])) is None:
+                del lis[-2].contents[1]
+                print("Kappa")
+
+            if lis[-2].find('i').find('a') is not None:  # if its in <a>...
                 band_dict['year1'] = lis[-2].contents[1]
-                band_dict['album1'] = lis[-2].find('a').get('title')
+                band_dict['album1'] = lis[-2].find('i').find('a').get('title')
             else:  # its in <i>
                 band_dict['year1'] = lis[-2].contents[1]
                 band_dict['album1'] = lis[-2].contents[0].get_text()
-            if lis[-4].find('a') is not None:
-                band_dict['year2'] = lis[-4].contents[1]
-                band_dict['album2'] = lis[-4].find('a').get('title')
-            else:
-                band_dict['year2'] = lis[-4].contents[1]
-                band_dict['album2'] = lis[-4].contents[0].get_text()
+            try:
+                if lis[-4].find('a') is not None:
+                    band_dict['year2'] = lis[-4].contents[1]
+                    band_dict['album2'] = lis[-4].find('i').find('a').get('title')
+                else:
+                    band_dict['year2'] = lis[-4].contents[1]
+                    band_dict['album2'] = lis[-4].contents[0].get_text()
+            except IndexError as e:
+                band_dict['year2'] = 'NONE'
+                band_dict['album2'] = 'NONE'
+                print(str(e) + " inserting NONE")
+
+    band_dict['year1'] = re.findall('\d{4}', band_dict['year1'])[0]
+    if band_dict['year2'] is not 'NONE':
+        band_dict['year2'] = re.findall('\d{4}', band_dict['year2'])[0]
 
     dictionaries.append(band_dict)
+    print(band_dict['band'] + ": ")
+    print("    " + band_dict['album1'] + ", " + band_dict['year1'])
+    print("    " + band_dict['album2'] + ", " + band_dict['year2'])
 
-for item in dictionaries:
-    print(item['band'] + ": ")
-    print("    " + item['album1'] + ", " + item['year1'])
-    print("    " + item['album2'] + ", " + item['year2'])
+# for item in dictionaries:
+#     print(item['band'] + ": ")
+#     print("    " + item['album1'] + ", " + item['year1'])
+#     print("    " + item['album2'] + ", " + item['year2'])
 
-# todo: only extract after Studio albums or Albums header, not right after discography
-# todo: osetrit, ak kapela ma iba 1 album (nemozem ist na [-4])
+# todo: (DONE - needs testing) only extract after Studio albums or Albums header, not right after discography
 # todo: test on all bands
