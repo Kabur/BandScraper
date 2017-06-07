34092901ba67c293e9eedf924fd687fb75d782d0
Kabur <tomasfabry1@gmail.com>
2017-06-07 13:33:39 +0200

    More bands. Currently fixing: Periphery

--- BandScraper.py
+++ BandScraper.py
@@ -1,3 +1,4 @@
+import random
 from time import sleep
 import sys
 import bs4 as bs
@@ -23,6 +24,9 @@ and they lived at the bottom of a well.
 </body>
 </html>
 """
+# test_soup = bs.BeautifulSoup(html_doc, 'lxml')
+# print(test_soup.find('body').contents)
+# exit()
 
 # print(soup.find_all('a', id='link2'))
 # print(soup.find_all(href=re.compile("example"), limit=1))
@@ -35,156 +39,215 @@ and they lived at the bottom of a well.
 # req = urllib.request.Request(url, headers=headers)
 # source = urllib.request.urlopen(req)
 # soup = bs.BeautifulSoup(source, 'lxml')
-
-
-with open("all_bands.txt", 'r') as file:
-    bands = file.readlines()
-
-# albums = []
-dictionaries = []
-for band in bands:
-    # sleep(3)
-    band_dict = {'band': band[:-1]}
-    # query = "site:en.wikipedia.org+" + band + "band"
-    # query = query.replace(" ", "+")
-    # response = requests.get("https://www.google.com/search?q=" + query)
-    # soup = bs.BeautifulSoup(response.text, "lxml")
-    #
-    # link = soup.find('h3', attrs={'class': 'r'}) # Guides BS to h3 class "r" where green Wikipedia URLs are located, then prints URLs
-    #
-    # link = link.find("a").get("href")
-    # link = link[7:].split("&")[0] # [7:] strips the /url?q= prefix, split removes garbage in link after &
-    link = 'https://en.wikipedia.org/wiki/Babymetal'
-    # link = 'https://en.wikipedia.org/wiki/Haken_(band)'
-    # link = 'https://en.wikipedia.org/wiki/Ayreon'
-    # link = 'https://en.wikipedia.org/wiki/Opeth'
-    response = requests.get(link)
-    soup = bs.BeautifulSoup(response.content, 'lxml')
-
-    # ''' find discography element '''
-    discography = soup.find(id='Discography')
-    disco_parent = discography.find_parent()
-    # sibling = disco_parent.find_next_sibling()
-    # ''' Go sideways to find the table with albums '''
-    # while sibling is not None and sibling.name != "table":
-    #     sibling = sibling.find_next_sibling()
-
-    sibling = disco_parent
-    sibling = sibling.find_next_sibling()
-    text = ""
-
+def find_album_table(sibling):
     while sibling is not None:
         # its a subheader like "Live Albums" - skip two times or skip once and break
         if sibling.name == "h3":
-            text = sibling.find('span').get_text()
-            if ("albums" not in text and "Albums" not in text) or "Live" in text:
+            text = sibling.find('span').get_text().lower()
+            if ("albums" not in text and band not in text) or "live" in text:
                 sibling = sibling.find_next_sibling()
-                continue
             else:
+                band_dict['band'] = band_dict['band'] + ' ' + text
                 sibling = sibling.find_next_sibling()
-                break
+                return sibling
         # There was no more junk or subheaders
         elif sibling.name == 'ul':
-            break
+            return sibling
+        elif sibling.name == 'div':
+            inside_div = find_album_table(sibling.contents[0])
+            if inside_div is not None:
+                return inside_div
         sibling = sibling.find_next_sibling()
-    else:
-        print("REACHED END OF FILE, NO ALBUMS FOUND LUL")
 
-    album_table = sibling
+    return sibling
 
-    # while ("albums" not in text and "Albums" not in text) or "Live" in text:
-    #     sibling = sibling.find_next_sibling()
-    #     if sibling.find('span') is not None:
-    #         text = sibling.find('span').get_text()
-    #     else:
-    #         text = ""
 
-    ''' 
-    Type 1:
-    Table is present 
-    '''
-    if album_table.name == "table":
-        table_rows = album_table.contents
-        # in case there is a junk table row -> cut it
-        if table_rows[-2].find("th") is None:
-            table_rows = table_rows[:-2]
+def find_wiki_link(string):
+    query = "site:en.wikipedia.org+" + string
+    query = query.replace(" ", "+")
+    response = requests.get("https://www.google.com/search?q=" + query)
+    soup = bs.BeautifulSoup(response.text, "lxml")
 
-        # i_tag = table_rows[-2].find('i')
-        # if i_tag is None:
-        #     table_rows = table_rows[:-2]
-        #     i_tag = table_rows[-2].find('i')
+    link = soup.find('h3', attrs={'class': 'r'}) # Guides BS to h3 class "r" where green Wikipedia URLs are located, then prints URLs
 
-        th = table_rows[-2].find('th')
-        # if its in <a>
-        if th.find('i').find("a") is not None:
-            band_dict['album1'] = th.find('i').find("a").get('title')
-            # if its just in <i>
-        else:
-            band_dict['album1'] = th.find('i').get_text()
-        td = th.find_next_sibling()
-        band_dict['year1'] = re.findall('\d{4}', td.get_text())[0]
+    link = link.find("a").get("href")
+    link = link[7:].split("&")[0] # [7:] strips the /url?q= prefix, split removes garbage in link after &
 
-        # ''' same shit for the second album, should put it to a function prolly xD '''
-        try:
-            th = table_rows[-4].find('th')
-            # if its in <a>
-            if th.find('i').find("a") is not None:
-                band_dict['album2'] = th.find('i').find("a").get('title')
-                # if its just in <i>
-            else:
-                band_dict['album2'] = th.find('i').get_text()
-            td = th.find_next_sibling()
-            band_dict['year2'] = re.findall('\d{4}', td.get_text())[0]
-        except IndexError as e:
-            band_dict['year2'] = 'NONE'
-            band_dict['album2'] = 'NONE'
-            print(str(e))
+    return link
+
+with open("all_bands_withlinks.txt", 'r') as file:
+    bands = file.readlines()
+    links = []
+
+for i, band in enumerate(bands):
+    links.append(band.split(',')[1][:-1])
+    bands[i] = band.split(',')[0].lower()
+
+# albums = []
+dictionaries = []
+for i, band in enumerate(bands):
+    if band != 'periphery':
+        continue
+    # fix_list = [26, 38, 40, 45, 48, 50, 51, 53, 59, 61, 62, 66, 67, 68, 70, 71, 72, 74, 77, 79, 82, 83, 84, 88, 89]
+    # skip = True
+    # for item in fix_list:
+    #     if i == item:
+    #         skip = False
+    #         break
+    # if skip:
+    #     continue
+    try:
+
+        band_dict = {'band': band}
+        # link = find_wiki_link(band + ' band')
+        link = links[i]
+        response = requests.get(link)
+        soup = bs.BeautifulSoup(response.content, 'lxml')
+
+        # ''' find discography element '''
+        discography = soup.find(id='Discography')
+        disco_parent = discography.find_parent()
+
+        # ''' Go sideways to find the table with albums '''
+        album_table = find_album_table(disco_parent.find_next_sibling())
 
         ''' 
-        TYPE 2:
-        No table, just <ul> and <li> tags
+        Type 1:
+        Table is present 
         '''
-    else:
-        # sibling = disco_parent.find_next_sibling()
+        if album_table.name == "table":
+            table_rows = album_table.contents
+            # table_rows = album_table.find_all('tr')
+            #
+            # row1 = table_rows[0].contents[1].get_text().lower()
 
-        # ''' Go sideways to find uls '''
-        while album_table is not None and album_table.name != "ul":
-            album_table = album_table.find_next_sibling()
-        if album_table is None:
-            print("DIDNT FIND AN ALBUM, INSPECT WIKI")
-        else:
-            lis = album_table.contents
+            # in case there is a junk table row -> cut it
+            i_tag = table_rows[-2].find('i')
+            if i_tag is None:
+                table_rows = table_rows[:-2]
+                i_tag = table_rows[-2].find('i')
 
-            # sometimes there is a junk tag instead of the year -> cut
-            if re.match('.*\d{4}.*', str(lis[-2].contents[1])) is None:
-                del lis[-2].contents[1]
-                print("Kappa")
+            # th = table_rows[-2].find('th')
+            column1 = i_tag.parent  # todo: this is kinda lame.
+            # test = album_table.find_all('tr')
+            # print(test)
+            # exit()
+            # if its in <a>
+            if i_tag.find("a") is not None:
+                band_dict['album1'] = i_tag.find("a").get('title')
+                # if its just in <i>
+            else:
+                band_dict['album1'] = i_tag.get_text()
+            # td = th.find_next_sibling()
+            column2 = column1.find_next_sibling()
+            band_dict['year1'] = re.findall('\d{4}', column2.get_text())[0]
 
-            if lis[-2].find('i').find('a') is not None:  # if its in <a>...
-                band_dict['year1'] = lis[-2].contents[1]
-                band_dict['album1'] = lis[-2].find('i').find('a').get('title')
-            else:  # its in <i>
-                band_dict['year1'] = lis[-2].contents[1]
-                band_dict['album1'] = lis[-2].contents[0].get_text()
+            # ''' same shit for the second album, should put it to a function prolly xD '''
             try:
-                if lis[-4].find('a') is not None:
-                    band_dict['year2'] = lis[-4].contents[1]
-                    band_dict['album2'] = lis[-4].find('i').find('a').get('title')
+                i_tag = table_rows[-4].find('i')
+                column1 = i_tag.parent
+                # th = table_rows[-4].find('th')
+                # if its in <a>
+                if i_tag.find("a") is not None:
+                    band_dict['album2'] = i_tag.find("a").get('title')
+                    # if its just in <i>
                 else:
-                    band_dict['year2'] = lis[-4].contents[1]
-                    band_dict['album2'] = lis[-4].contents[0].get_text()
+                    band_dict['album2'] = i_tag.get_text()
+
+                column2 = column1.find_next_sibling()
+                band_dict['year2'] = re.findall('\d{4}', column2.get_text())[0]
             except IndexError as e:
                 band_dict['year2'] = 'NONE'
                 band_dict['album2'] = 'NONE'
-                print(str(e) + " inserting NONE")
+                print(str(e))
 
-    band_dict['year1'] = re.findall('\d{4}', band_dict['year1'])[0]
-    if band_dict['year2'] is not 'NONE':
-        band_dict['year2'] = re.findall('\d{4}', band_dict['year2'])[0]
+            ''' 
+            TYPE 2:
+            No table, just <ul> and <li> tags
+            '''
+        else:
+            # sibling = disco_parent.find_next_sibling()
 
-    dictionaries.append(band_dict)
-    print(band_dict['band'] + ": ")
-    print("    " + band_dict['album1'] + ", " + band_dict['year1'])
-    print("    " + band_dict['album2'] + ", " + band_dict['year2'])
+            # ''' Go sideways to find uls '''
+            while album_table is not None and album_table.name != "ul":
+                album_table = album_table.find_next_sibling()
+            if album_table is None:
+                print("DIDNT FIND AN ALBUM, INSPECT WIKI")
+            else:
+                lis = album_table.contents
 
-# todo: test on all bands
+                # sometimes there is a junk tag instead of the year -> cut
+                if len(lis[-2].contents) > 1:
+                    if re.match('.*\d{4}.*', str(lis[-2].contents[1])) is None:
+                        del lis[-2].contents[1]
+                        print("deleting junk line...")
+
+                # if its in <a>...
+                if lis[-2].find('i').find('a') is not None:
+                    if len(lis[-2].contents) > 1:
+                        band_dict['year1'] = lis[-2].contents[1]
+                    else:
+                        band_dict['year1'] = 'NONE'
+
+                    band_dict['album1'] = lis[-2].find('i').find('a').get('title')
+                # its in <i>
+                else:
+                    if len(lis[-2].contents) > 1:
+                        band_dict['year1'] = lis[-2].contents[1]
+                    else:
+                        band_dict['year1'] = 'NONE'
+
+                    band_dict['album1'] = lis[-2].contents[0].get_text()
+                # REPEAT FOR SECOND ALBUM
+                try:
+                    if lis[-4].find('a') is not None:
+                        if len(lis[-4].contents) > 1:
+                            band_dict['year2'] = lis[-4].contents[1]
+                        else:
+                            band_dict['year2'] = 'NONE'
+
+                        band_dict['album2'] = lis[-4].find('i').find('a').get('title')
+                    else:
+                        if len(lis[-4].contents) > 1:
+                            band_dict['year2'] = lis[-4].contents[1]
+                        else:
+                            band_dict['year2'] = 'NONE'
+
+                        band_dict['album2'] = lis[-4].contents[0].get_text()
+                except IndexError as e:
+                    band_dict['year2'] = 'NONE'
+                    band_dict['album2'] = 'NONE'
+                    print(str(e) + " inserting NONE")
+
+        if band_dict['year1'] is not 'NONE':
+            band_dict['year1'] = re.findall('\d{4}', band_dict['year1'])[0]
+        if band_dict['year2'] is not 'NONE':
+            band_dict['year2'] = re.findall('\d{4}', band_dict['year2'])[0]
+
+        dictionaries.append(band_dict)
+        print(str(i) + '. ' + band_dict['band'] + ": ")
+        print("    " + band_dict['album1'] + ", " + band_dict['year1'])
+        print("    " + band_dict['album2'] + ", " + band_dict['year2'])
+    except Exception as e:
+        print(band + ' went to shit, LUL')
+        print(str(e))
+    sleep(random.randrange(3, 5))
+
+# todo: example: devin townsend - extract solo albums as well as devin townsend project albums
+''' No Wiki:
+ForTiorI
+igorrr
+pryapisme
+roadrunner united(no discography header) 
+corpo-mente 
+darth vegas 
+hentai corporation
+
+Wrong wiki:
+shaolin death squad
+dead brothers
+duct tape mustache
+
+Has headers but none contain "albums"
+eminem
+'''
